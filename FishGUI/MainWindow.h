a8060d09a8267265e0fda059d26ba8e13587a126
BGWoodward <bgwoodward@gmail.com>
2015-09-07 14:15:31 -0400

    functionality and style update
    put in signals and functions for adding fish and a vector structure to
    mainwindow. Also made the controls a dockwidget. Did not build yet, very
    experimental.

--- FishGUI/MainWindow.h
+++ FishGUI/MainWindow.h
@@ -3,6 +3,8 @@
 #include <QMessageBox>
 #include <player.h>
 #include "ui_mainwindow.h"
+#include "fish.h"
+#include <vector>
 
 namespace Ui {
 	class MainWidget;
@@ -22,7 +24,13 @@ class MainWindow : public QWidget
   	void on_LoadVideo_clicked();
 	void on_SpeedUp_clicked();
 	void on_SlowDown_clicked();
+	void on_addFlounder_clicked();
+	void on_addCod_clicked()
+	void on_addHaddock_clicked();
+	void on_addSkate_clicked();
  private:
  	Ui::MainWidget *ui;
  	Player* myPlayer;
+	Fish* tempFish;
+	vector<Fish> myFishList;
 };
