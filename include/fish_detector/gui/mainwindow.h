d41e02c506dfc14c89fc2110c6eb81b83561361f
Jonathan Takahashi <jtakahashi@gmail.com>
2016-09-29 19:12:56 -0400

    Fix indentation

--- include/fish_detector/gui/mainwindow.h
+++ include/fish_detector/gui/mainwindow.h
@@ -30,79 +30,79 @@
 namespace fish_detector { namespace gui {
 
 namespace Ui {
-	class MainWidget;
+  class MainWidget;
 }
 
 class MainWindow : public QWidget
 {
-	Q_OBJECT
+  Q_OBJECT
 
- public:
- 	explicit MainWindow(QWidget *parent = 0);
-// 	~MainWindow();
+  public:
+  explicit MainWindow(QWidget *parent = 0);
+//   ~MainWindow();
 
-    void nextFrame();
-    void prevFrame();
+  void nextFrame();
+  void prevFrame();
 
  private slots:
-  	void updatePlayerUI(QImage img);
-	void updateSubTypeMenu(int typeIndex);
-    void goToFish();
-	QString getFormattedTime(int timeInSeconds);
-	void on_videoSlider_sliderPressed();
-	void on_videoSlider_sliderReleased();
-	void on_videoSlider_sliderMoved(int position);
-  	void on_Play_clicked();
-  	void on_LoadVideo_clicked();
-    void on_loadAnnotate_clicked();
-    void on_saveAnnotate_clicked();
-	void on_SpeedUp_clicked();
-	void on_SlowDown_clicked();
-    void on_Rewind_clicked();
-	void on_minusOneFrame_clicked();
-	void on_plusOneFrame_clicked();
-	void on_addRound_clicked();
-	void on_addFlat_clicked();
-	void on_addSkate_clicked();
-	void on_addOther_clicked();
-    void on_prevFish_clicked();
-    void on_nextFish_clicked();
-    void on_goToFrame_clicked();
-    void on_removeFish_clicked();
-    void on_addRegion_clicked();
-    void on_removeRegion_clicked();
-    void on_nextAndCopy_clicked();
-    void on_typeMenu_currentIndexChanged(int tIdx);
-    void on_subTypeMenu_currentIndexChanged(int sIdx);
+  void updatePlayerUI(QImage img);
+  void updateSubTypeMenu(int typeIndex);
+  void goToFish();
+  QString getFormattedTime(int timeInSeconds);
+  void on_videoSlider_sliderPressed();
+  void on_videoSlider_sliderReleased();
+  void on_videoSlider_sliderMoved(int position);
+  void on_Play_clicked();
+  void on_LoadVideo_clicked();
+  void on_loadAnnotate_clicked();
+  void on_saveAnnotate_clicked();
+  void on_SpeedUp_clicked();
+  void on_SlowDown_clicked();
+  void on_Rewind_clicked();
+  void on_minusOneFrame_clicked();
+  void on_plusOneFrame_clicked();
+  void on_addRound_clicked();
+  void on_addFlat_clicked();
+  void on_addSkate_clicked();
+  void on_addOther_clicked();
+  void on_prevFish_clicked();
+  void on_nextFish_clicked();
+  void on_goToFrame_clicked();
+  void on_removeFish_clicked();
+  void on_addRegion_clicked();
+  void on_removeRegion_clicked();
+  void on_nextAndCopy_clicked();
+  void on_typeMenu_currentIndexChanged(int tIdx);
+  void on_subTypeMenu_currentIndexChanged(int sIdx);
 public:
-    void updateImage(const QImage &image);
-    std::unique_ptr<Player> &getPlayer() { return player; }
-    
+  void updateImage(const QImage &image);
+  std::unique_ptr<Player> &getPlayer() { return player; }
+  
 private:
-    void updateAnnotations();
-    void addFish(FishTypeEnum fType);
-    void updateVecIndex();
-    FishTypeEnum getFishType (std::string const &inString);
-    int getFishSpecies (FishTypeEnum fType, std::string const &sString);
-    std::string getFishTypeString (FishTypeEnum fType);
-    std::string getFishSpeciesString (FishTypeEnum fType, int species);
-    void disableControls();
-    void enableControls();
-    void keyPressEvent(QKeyEvent *e);
-    void removeRegion(std::uint64_t id, std::uint64_t frame);
-    void processAnnotations(uint64_t frame);
+  void updateAnnotations();
+  void addFish(FishTypeEnum fType);
+  void updateVecIndex();
+  FishTypeEnum getFishType (std::string const &inString);
+  int getFishSpecies (FishTypeEnum fType, std::string const &sString);
+  std::string getFishTypeString (FishTypeEnum fType);
+  std::string getFishSpeciesString (FishTypeEnum fType, int species);
+  void disableControls();
+  void enableControls();
+  void keyPressEvent(QKeyEvent *e);
+  void removeRegion(std::uint64_t id, std::uint64_t frame);
+  void processAnnotations(uint64_t frame);
 private:
-    void updateTypeMenu();
-    std::unique_ptr<Document> document;
-    std::unique_ptr<Ui::MainWidget> ui;
-    std::unique_ptr<Player> player;
-    std::vector<Fish> myFishList;
-    std::vector<Fish>::iterator listPos;
-    std::unique_ptr<QGraphicsScene> scene;
-    int fIndex;
-    int nextID;
-    QGraphicsPixmapItem *displayImage;
-    std::list<AnnotatedRegion *> currentAnnotations;
+  void updateTypeMenu();
+  std::unique_ptr<Document> document;
+  std::unique_ptr<Ui::MainWidget> ui;
+  std::unique_ptr<Player> player;
+  std::vector<Fish> myFishList;
+  std::vector<Fish>::iterator listPos;
+  std::unique_ptr<QGraphicsScene> scene;
+  int fIndex;
+  int nextID;
+  QGraphicsPixmapItem *displayImage;
+  std::list<AnnotatedRegion *> currentAnnotations;
 };
 
 template<class T>
