cc8930df1bfc8adc4c4a5f450a34669e67cea25d
Jonathan Takahashi <jtakahashi@gmail.com>
2016-09-30 00:40:20 -0400

    Find qtcore, opengl, and windows libs

--- cmake/FishDetFindLibs.cmake
+++ cmake/FishDetFindLibs.cmake
@@ -3,6 +3,10 @@ find_package( Qt5Widgets )
 if( NOT Qt5Widgets_FOUND )
   message( FATAL_ERROR "Could not find Qt5Widgets.  Build cannot continue." )
 endif()
+find_package( Qt5Core )
+if( NOT Qt5Core_FOUND )
+  message( FATAL_ERROR "Could not find Qt5Core.  Build cannot continue." )
+endif()
 
 # --- Boost ---
 find_package( Boost )
@@ -15,3 +19,21 @@ find_package( OpenCV )
 if( NOT OpenCV_FOUND )
   message( FATAL_ERROR "Could not find OpenCV.  Build cannot continue." )
 endif()
+
+# --- OpenGL ---
+find_package( OpenGL )
+if( NOT OPENGL_FOUND )
+  message( FATAL_ERROR "Could not find OpenGL.  Build cannot continue." )
+endif()
+
+# --- Windows ---
+if( MSVC )
+  set( WINDOWS_LIBRARIES
+    winmm
+    UxTheme
+    ws2_32
+    imm32
+    Dwmapi
+    )
+endif()
+
