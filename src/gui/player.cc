a1e151a9f76eca68c5a01c2826fdb91eae446cd5
Jonathan Takahashi <jtakahashi@Jonathans-MacBook-Air.local>
2016-10-09 14:37:01 -0400

    Conform to coding standard

--- src/gui/player.cc
+++ src/gui/player.cc
@@ -3,159 +3,159 @@
 namespace fish_detector { namespace gui {
 
 Player::Player(QObject *parent)
-	: QThread(parent)
+  : QThread(parent)
 {
-    frameIndex = 0;
-	stop = true;
+  frameIndex = 0;
+  stop = true;
 }
 
 bool Player::loadVideo(std::string filename) {
-    capture.reset(new cv::VideoCapture(filename));
-	if (capture->isOpened())
-	{
-        frameRate = capture->get(CV_CAP_PROP_FPS);
-        currentSpeed = 100;
-		delay = (1000/frameRate);
-		return true;
-	}
-	else
-		return false;
+  capture.reset(new cv::VideoCapture(filename));
+  if (capture->isOpened())
+  {
+    frameRate = capture->get(CV_CAP_PROP_FPS);
+    currentSpeed = 100;
+    delay = (1000/frameRate);
+    return true;
+  }
+  else
+    return false;
 }
 
 void Player::Play()
 {
-	if (!isRunning()) 
-	{
-		if (isStopped())
-		{
-			stop = false;
-		}
-		start(LowPriority);
-	}
+  if (!isRunning()) 
+  {
+    if (isStopped())
+    {
+      stop = false;
+    }
+    start(LowPriority);
+  }
 }
 
 void Player::run()
 {
-	while(!stop) {
-		if (!capture->read(frame))
-		{
-			stop = true;
-		}
-        frameIndex++;
-		if (frame.channels() == 3)
-		{
-			cv::cvtColor(frame, RGBframe, CV_BGR2RGB);
-			img = QImage((const unsigned char*)(RGBframe.data),
-				RGBframe.cols, RGBframe.rows, QImage::Format_RGB888);
-		}
-		else
-		{
-			img = QImage((const unsigned char*)(frame.data),
-				frame.cols, frame.rows, QImage::Format_Indexed8);
-		}
-		emit processedImage(img);
-		this->msleep(delay);
-	}
+  while(!stop) {
+    if (!capture->read(frame))
+    {
+      stop = true;
+    }
+    frameIndex++;
+    if (frame.channels() == 3)
+    {
+      cv::cvtColor(frame, RGBframe, CV_BGR2RGB);
+      img = QImage((const unsigned char*)(RGBframe.data),
+        RGBframe.cols, RGBframe.rows, QImage::Format_RGB888);
+    }
+    else
+    {
+      img = QImage((const unsigned char*)(frame.data),
+        frame.cols, frame.rows, QImage::Format_Indexed8);
+    }
+    emit processedImage(img);
+    this->msleep(delay);
+  }
 }
 
 QImage Player::getOneFrame()
 {
-	capture->read(frame);
-	if (frame.channels() == 3)
-	{
-		cv::cvtColor(frame, RGBframe, CV_BGR2RGB);
-		img = QImage((const unsigned char*)(RGBframe.data),
-			RGBframe.cols, RGBframe.rows, QImage::Format_RGB888);
-	}
-	else
-	{
-		img = QImage((const unsigned char*)(frame.data),
-			frame.cols, frame.rows, QImage::Format_Indexed8);
-	}
-	return img;
+  capture->read(frame);
+  if (frame.channels() == 3)
+  {
+    cv::cvtColor(frame, RGBframe, CV_BGR2RGB);
+    img = QImage((const unsigned char*)(RGBframe.data),
+      RGBframe.cols, RGBframe.rows, QImage::Format_RGB888);
+  }
+  else
+  {
+    img = QImage((const unsigned char*)(frame.data),
+      frame.cols, frame.rows, QImage::Format_Indexed8);
+  }
+  return img;
 }
 
 Player::~Player()
 {
-    if (1)
-    {
-        QMutexLocker locker(&mutex);
-        stop = true;
-        //capture->release();
-        condition.wakeOne();
-        wait();
-    }
+  if (1)
+  {
+    QMutexLocker locker(&mutex);
+    stop = true;
+    //capture->release();
+    condition.wakeOne();
+    wait();
+  }
 }
 void Player::Stop()
 {
-	stop = true;
+  stop = true;
 }
 
 void Player::msleep(int ms)
 {
-    std::this_thread::sleep_for(std::chrono::microseconds(ms*1000));
+  std::this_thread::sleep_for(std::chrono::microseconds(ms*1000));
 }
 
 bool Player::isStopped() const{
-	return this->stop;
+  return this->stop;
 }
 
 void Player::speedUp()
 {
-	delay = delay/2;
-	if (delay<1)
-	  delay = 1;
-    currentSpeed = currentSpeed * 2;
+  delay = delay/2;
+  if (delay<1)
+    delay = 1;
+  currentSpeed = currentSpeed * 2;
 }
 
 void Player::slowDown()
 {
-	delay = delay*2;
-    currentSpeed = currentSpeed / 2;
+  delay = delay*2;
+  currentSpeed = currentSpeed / 2;
 }
 
 QImage Player::nextFrame() {
-    setCurrentFrame(++frameIndex);
-    return getOneFrame();
+  setCurrentFrame(++frameIndex);
+  return getOneFrame();
 }
 
 QImage Player::prevFrame() {
-    setCurrentFrame(--frameIndex);
-    return getOneFrame();
+  setCurrentFrame(--frameIndex);
+  return getOneFrame();
 }
 
 std::int64_t Player::getCurrentFrame()
 {
-    return frameIndex;
+  return frameIndex;
 }
 
 double Player::getNumberOfFrames()
 {
-	return capture->get(CV_CAP_PROP_FRAME_COUNT);
+  return capture->get(CV_CAP_PROP_FRAME_COUNT);
 }
 
 double Player::getFrameRate()
 {
-	return frameRate;
+  return frameRate;
 }
 
 void Player::setCurrentFrame(std::int64_t frameNumber)
 {
-    capture->set(CV_CAP_PROP_POS_MSEC, (double(frameNumber)/double(frameRate)*1000));
-    //capture->set(CV_CAP_PROP_POS_FRAMES, frameNumber);
-    frameIndex = frameNumber;
+  capture->set(CV_CAP_PROP_POS_MSEC, (double(frameNumber)/double(frameRate)*1000));
+  //capture->set(CV_CAP_PROP_POS_FRAMES, frameNumber);
+  frameIndex = frameNumber;
 }
 
 QImage Player::setFrame(std::int64_t frame)
 {
-    setCurrentFrame(frame);
-    return getOneFrame();
+  setCurrentFrame(frame);
+  return getOneFrame();
 }
 
 
 double Player::getCurrentSpeed()
 {
-    return currentSpeed;
+  return currentSpeed;
 }
 
 #include "../../include/fish_detector/gui/moc_player.cpp"
