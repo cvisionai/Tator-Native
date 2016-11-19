d34b1350b118b548b372af3ba5da5b5732deb33e
Jonathan Takahashi <jtakahashi@gmail.com>
2016-11-19 15:43:17 -0500

    Add content to second test

--- test/video_annotator/test_video_annotator.cc
+++ test/video_annotator/test_video_annotator.cc
@@ -9,9 +9,14 @@ Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin)
 
 void TestVideoAnnotator::testLoadVideo() {
   fish_detector::video_annotator::MainWindow mainwin;
-  mainwin.player_->loadVideo("slow_motion_drop.mp4");
+  std::string name = "C:/local/FishDetector/test/video_annotator/slow_motion_drop.mp4";
+  QVERIFY(mainwin.player_->loadVideo(name));
 }
 void TestVideoAnnotator::testSaveAnnotationsNoFish() {
+  fish_detector::video_annotator::MainWindow mainwin;
+  std::string filename = "slow_motion_drop.mp4";
+  QVERIFY(mainwin.player_->loadVideo(filename));
+  mainwin.onLoadVideoSuccess(QString::fromUtf8(filename.c_str()));
 }
 
 QTEST_MAIN(TestVideoAnnotator)
