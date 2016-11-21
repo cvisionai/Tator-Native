496d9ed12fb91db31bfbf737af680def8bf786b4
Jonathan Takahashi <jtakahashi@gmail.com>
2016-11-20 23:08:39 -0500

    Implement most functions in image annotation classes

--- src/image_annotator/image_annotation.cc
+++ src/image_annotator/image_annotation.cc
@@ -25,10 +25,27 @@ ImageAnnotation::ImageAnnotation()
 
 pt::ptree ImageAnnotation::write() const {
   pt::ptree tree;
+  tree.put("image_file", image_file_);
+  tree.put("species", species_);
+  tree.put("subspecies", subspecies_);
+  tree.put("id", id_);
+  tree.put("x", rect_.x);
+  tree.put("y", rect_.y);
+  tree.put("w", rect_.w);
+  tree.put("h", rect_.h);
   return tree;
 }
 
 void ImageAnnotation::read(const pt::ptree &tree) {
+  image_file_ = tree.get<std::string>("image_file");
+  species_ = tree.get<std::string>("species");
+  subspecies_ = tree.get<std::string>("subspecies");
+  id_ = tree.get<uint64_t>("id");
+  uint64_t x = tree.get<uint64_t>("x");
+  uint64_t y = tree.get<uint64_t>("y");
+  uint64_t w = tree.get<uint64_t>("w");
+  uint64_t h = tree.get<uint64_t>("h");
+  rect_ = Rect(x, y, w, h);
 }
 
 ImageAnnotationList::ImageAnnotationList()
@@ -38,9 +55,22 @@ ImageAnnotationList::ImageAnnotationList()
 }
 
 void ImageAnnotationList::insert(const ImageAnnotation &annotation) {
+  list_.push_front(annotation);
+  by_file_.insert({annotation.image_file_, list_.begin()});
+  by_species_.insert({{annotation.species_, annotation.subspecies_}, 
+                      list_.begin()});
 }
 
 void ImageAnnotationList::remove(const std::string &image_file, uint64_t id) {
+  auto it = by_file_.left.find(image_file);
+  for(; it != by_file_.left.end(); it++) {
+    if(it->second->id_ == id) {
+      list_.erase(it->second);
+      by_species_.right.erase(by_species_.right.find(it->second));
+      by_file_.left.erase(it);
+      break;
+    }
+  }
 }
 
 void ImageAnnotationList::write() const {
