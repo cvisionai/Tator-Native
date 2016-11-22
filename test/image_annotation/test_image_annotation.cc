32acaeb951f85874ab8c116ed0c242f5fd34082d
Jonathan Takahashi <jtakahashi@gmail.com>
2016-11-21 19:45:37 -0500

    Add unit test for image annotation

---
+++ test/image_annotation/test_image_annotation.cc
@@ -0,0 +1,34 @@
+#include "fish_detector/common/rect.h"
+#include "fish_detector/image_annotator/image_annotation.h"
+#include "test_image_annotation.h"
+
+namespace ia = fish_detector::image_annotator;
+
+void TestImageAnnotation::testSerialize() {
+  std::vector<std::string> image_list = {
+    "ceres/ceres1.png",
+    "ceres/ceres2.jpg",
+    "ceres/ceres3.jpg",
+    "ceres/ceres4.jpg"};
+  ia::ImageAnnotationList list;
+  list.insert(ia::ImageAnnotation(
+    image_list[0], "asdfa", "ljklg", 0, 
+    fish_detector::Rect(1, 2, 3, 4)));
+  list.insert(ia::ImageAnnotation(
+    image_list[0], "ahsdifuasega", "ljsdasdhaklg", 1, 
+    fish_detector::Rect(3, 2, 1, 9)));
+  list.insert(ia::ImageAnnotation(
+    image_list[1], "ahspda8", "asha", 0, 
+    fish_detector::Rect(4, 3, 2, 8)));
+  list.insert(ia::ImageAnnotation(
+    image_list[3], "ahspda8", "ashaahsgha", 0, 
+    fish_detector::Rect(4, 3, 2, 8)));
+  list.write(image_list);
+  ia::ImageAnnotationList deserialized_list;
+  deserialized_list.read(image_list);
+  QVERIFY(list == deserialized_list);
+}
+
+QTEST_MAIN(TestImageAnnotation)
+#include "test_image_annotation.moc"
+
---
+++ test/image_annotation/test_image_annotation.h
@@ -0,0 +1,7 @@
+#include <QtTest/QtTest>
+
+class TestImageAnnotation : public QObject {
+  Q_OBJECT
+private slots:
+  void testSerialize();
+};
