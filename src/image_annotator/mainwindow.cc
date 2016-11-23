f7f8e708d7097e7bd8dd5d3ecc888934b02842aa
Jonathan Takahashi <jtakahashi@gmail.com>
2016-11-22 22:44:32 -0500

    Add implementations for loading image directories and navigating images

--- src/image_annotator/mainwindow.cc
+++ src/image_annotator/mainwindow.cc
@@ -1,11 +1,28 @@
+#include <boost/filesystem.hpp>
+
+#include <QFileDialog>
+#include <QMessageBox>
+
 #include "fish_detector/image_annotator/mainwindow.h"
 #include "ui_mainwindow.h"
 
 namespace fish_detector { namespace image_annotator {
 
+namespace fs = boost::filesystem;
+
+namespace { //anonymous
+
+static const std::vector<std::string> kDirExtensions = {
+  ".jpg", ".png", ".bmp", ".tif"};
+
+} // anonymous namespace
+
 MainWindow::MainWindow(QWidget *parent)
-  : annotations_(new ImageAnnotationList)
-  , ui_(new Ui::MainWidget) {
+  : annotations_(nullptr)
+  , scene_(new QGraphicsScene)
+  , pixmap_(nullptr)
+  , ui_(new Ui::MainWidget)
+  , image_files_() {
   ui_->setupUi(this);
   setStyleSheet("QPushButton { background-color: rgb(230, 230, 230);"
 	  "border-style: outset; border-radius: 5px; border-width: 2px; "
@@ -13,6 +30,10 @@ MainWindow::MainWindow(QWidget *parent)
 	  "QPushButton:pressed{background-color: rgb(190, 190, 190); "
     "border-style: outset; border-radius: 5px;"
 	  "border-width: 2px; border-color: grey; padding: 6px;}");
+  ui_->next->setEnabled(false);
+  ui_->prev->setEnabled(false);
+  ui_->saveAnnotations->setEnabled(false);
+  ui_->imageSlider->setEnabled(false);
 }
 
 void MainWindow::on_addSpecies_clicked() {
@@ -25,18 +46,71 @@ void MainWindow::on_saveSpecies_clicked() {
 }
 
 void MainWindow::on_next_clicked() {
+  int next_val = ui_->imageSlider->value() + 1;
+  if(next_val <= ui_->imageSlider->maximum()) {
+    ui_->imageSlider->setValue(next_val);
+  }
 }
 
 void MainWindow::on_prev_clicked() {
+  int prev_val = ui_->imageSlider->value() - 1;
+  if(prev_val >= ui_->imageSlider->minimum()) {
+    ui_->imageSlider->setValue(prev_val);
+  }
 }
 
 void MainWindow::on_loadImageDir_clicked() {
+  QString image_dir = QFileDialog::getExistingDirectory(this, 
+    "Select an image directory.");
+  if(!image_dir.isEmpty()) {
+    onLoadDirectorySuccess(image_dir);
+  }
 }
 
 void MainWindow::on_saveAnnotations_clicked() {
 }
 
-void MainWindow::on_imageSlider_sliderMoved() {
+void MainWindow::on_imageSlider_valueChanged() {
+  if(pixmap_ != nullptr) {
+    scene_->removeItem(pixmap_.get());
+  }
+  QString filename(image_files_[ui_->imageSlider->value()].c_str());
+  QImage current(filename);
+  if(!current.isNull()) {
+    pixmap_.reset(scene_->addPixmap(QPixmap::fromImage(current)));
+    scene_->setSceneRect(current.rect());
+    ui_->imageWindow->setScene(scene_.get());
+    ui_->imageWindow->fitInView(scene_->sceneRect(), Qt::KeepAspectRatio);
+    ui_->imageWindow->show();
+  }
+  else {
+    QMessageBox err;
+    err.critical(0, "Error", std::string(
+        std::string("Error loading image ")
+      + filename.toStdString()
+      + std::string(".")).c_str());
+  }
+}
+
+void MainWindow::onLoadDirectorySuccess(const QString &image_dir) {
+  image_files_.clear();
+  fs::directory_iterator dir_it(image_dir.toStdString());
+  fs::directory_iterator dir_end;
+  for(; dir_it != dir_end; ++dir_it) {
+    fs::path ext(dir_it->path().extension());
+    for(auto &ok_ext : kDirExtensions) {
+      if(ext == ok_ext) {
+        image_files_.push_back(dir_it->path().generic_string());
+      }
+    }
+  }
+  ui_->next->setEnabled(true);
+  ui_->prev->setEnabled(true);
+  ui_->saveAnnotations->setEnabled(true);
+  ui_->imageSlider->setEnabled(true);
+  ui_->imageSlider->setMinimum(0);
+  ui_->imageSlider->setMaximum(static_cast<int>(image_files_.size() - 1));
+  ui_->imageSlider->setSingleStep(1);
 }
 
 #include "../../include/fish_detector/image_annotator/moc_mainwindow.cpp"
