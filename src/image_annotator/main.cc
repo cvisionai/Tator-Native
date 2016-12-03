0917f69de720cc508cfb759cc4708737f2cd1a1b
Jonathan Takahashi <jtakahashi@gmail.com>
2016-12-03 17:13:07 -0500

    Load ico plugin for other executables so window icon works

--- src/image_annotator/main.cc
+++ src/image_annotator/main.cc
@@ -5,11 +5,6 @@
 
 #ifdef _WIN32
 Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin)
-#elif __APPLE__
-Q_IMPORT_PLUGIN(QCocoaIntegrationPlugin)
-#endif
-
-#ifdef _WIN32
 Q_IMPORT_PLUGIN(QDDSPlugin)
 Q_IMPORT_PLUGIN(QGifPlugin)
 Q_IMPORT_PLUGIN(QICNSPlugin)
@@ -20,6 +15,8 @@ Q_IMPORT_PLUGIN(QTgaPlugin)
 Q_IMPORT_PLUGIN(QTiffPlugin)
 Q_IMPORT_PLUGIN(QWbmpPlugin)
 Q_IMPORT_PLUGIN(QWebpPlugin)
+#elif __APPLE__
+Q_IMPORT_PLUGIN(QCocoaIntegrationPlugin)
 #endif
 
 int main(int argc, char* argv[])
