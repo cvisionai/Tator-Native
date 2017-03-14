8e7ed04824203d7f64bf165df247f16d3d14495e
Jonathan Takahashi <jtakahashi@Jonathans-MacBook-Air.local>
2017-03-14 14:55:18 -0400

    Insert a couple more detection annotations in unit test

--- test/video_annotation/test_video_annotation.cc
+++ test/video_annotation/test_video_annotation.cc
@@ -21,6 +21,8 @@ void TestVideoAnnotation::testSerialize() {
   ann.insert(std::make_shared<va::DetectionAnnotation>(
     1231, 1, fish_annotator::Rect(1, 2, 3, 4)));
   ann.insert(std::make_shared<va::DetectionAnnotation>(
+    918, 1, fish_annotator::Rect(1, 2, 3, 4)));
+  ann.insert(std::make_shared<va::DetectionAnnotation>(
     1031, 1, fish_annotator::Rect(1, 2, 3, 4)));
   ann.insert(std::make_shared<va::DetectionAnnotation>(
     1151, 1, fish_annotator::Rect(5, 2, 34, 2)));
@@ -28,13 +30,17 @@ void TestVideoAnnotation::testSerialize() {
     1151, 1, fish_annotator::Rect(15, 12, 3, 5)));
   ann.insert(std::make_shared<va::DetectionAnnotation>(
     1151, 3, fish_annotator::Rect(5, 2, 34, 2)));
+  ann.insert(std::make_shared<va::DetectionAnnotation>(
+    718, 1, fish_annotator::Rect(1, 2, 3, 4)));
   QVERIFY(ann.getAllSpecies().size() == 3);
-  QVERIFY(ann.detections_by_id_.left.count(1) == 3);
+  QVERIFY(ann.detections_by_id_.left.count(1) == 5);
   QVERIFY(ann.detections_by_id_.left.count(2) == 0);
   QVERIFY(ann.detections_by_id_.left.count(3) == 1);
   QVERIFY(ann.detections_by_frame_.left.count(1231) == 1);
   QVERIFY(ann.detections_by_frame_.left.count(1151) == 2);
   QVERIFY(ann.detections_by_frame_.left.count(1031) == 1);
+  QVERIFY(ann.detections_by_frame_.left.count(918) == 1);
+  QVERIFY(ann.detections_by_frame_.left.count(718) == 1);
   boost::filesystem::path csv_path("vid_ann.csv");
   ann.write(csv_path, 15, 23, "Joe Mango", "Open", 24.0);
   va::VideoAnnotation deser_ann;
@@ -45,7 +51,7 @@ void TestVideoAnnotation::testSerialize() {
   va::VideoAnnotation deser_ann_1;
   deser_ann_1.read(csv_path);
   QVERIFY(ann == deser_ann_1);
-  QVERIFY(ann.detections_by_id_.left.count(1) == 2);
+  QVERIFY(ann.detections_by_id_.left.count(1) == 4);
   ann.remove(1);
   ann.write(csv_path, 15, 23, "Joe Mango", "Open", 24.0);
   va::VideoAnnotation deser_ann_2;
