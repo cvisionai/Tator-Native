705539e7d33133712ef21d0c2ec6f62cf01167e0
Jonathan Takahashi <jtakahashi@gmail.com>
2017-04-09 11:26:34 -0400

    Add metadata struct

---
+++ include/fish_annotator/video_annotator/metadata.h
@@ -0,0 +1,31 @@
+/// @file
+/// @brief Defines Metadata struct.
+
+#ifndef METADATA_H
+#define METADATA_H
+
+#include <string>
+
+namespace fish_annotator { namespace video_annotator {
+
+/// @brief Stores video annotation metadata.
+struct Metadata {
+
+  /// @brief Base file name.
+  std::string file_name_;
+
+  /// @brief Reviewer name.
+  std::string reviewer_name_;
+
+  /// @brief Trip ID.
+  uint32_t trip_id_;
+
+  /// @brief Tow number.
+  uint32_t tow_number_;
+
+};
+
+}} // namespace fish_annotator::video_annotator
+
+#endif // METADATA_H
+
