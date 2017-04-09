2899d2ee057d850d32b3cdbaa7653fefa23357d7
Jonathan Takahashi <jtakahashi@gmail.com>
2017-04-09 11:26:56 -0400

    Add metadata dialog implementation

---
+++ src/video_annotator/metadata_dialog.cc
@@ -0,0 +1,56 @@
+#include <QMessageBox>
+
+#include "fish_annotator/video_annotator/metadata_dialog.h"
+#include "ui_metadata_dialog.h"
+
+namespace fish_annotator { namespace video_annotator {
+
+MetadataDialog::MetadataDialog(QWidget *parent)
+  : QDialog(parent)
+  , ui_(new Ui::MetadataDialog) {
+  ui_->setupUi(this);
+}
+
+void MetadataDialog::on_ok_clicked() {
+  QRegExp re("\\d*");
+  auto trip_id_valid = re.exactMatch(ui_->tripId->text());
+  auto tow_number_valid = re.exactMatch(ui_->towNumber->text());
+  if(trip_id_valid == false) {
+    QMessageBox msgBox;
+    msgBox.setText("Trip ID must be an integer!");
+    msgBox.exec();
+  }
+  if(tow_number_valid == false) {
+    QMessageBox msgBox;
+    msgBox.setText("Tow number must be an integer!");
+    msgBox.exec();
+  }
+  if(tow_number_valid && trip_id_valid) {
+    accept();
+  }
+}
+
+void MetadataDialog::on_cancel_clicked() {
+  reject();
+}
+
+Metadata MetadataDialog::getMetadata() {
+  Metadata metadata;
+  metadata.file_name_ = ui_->fileName->text().toStdString();
+  metadata.reviewer_name_ = ui_->reviewerName->text().toStdString();
+  metadata.trip_id_ = ui_->tripId->text().toInt();
+  metadata.tow_number_ = ui_->towNumber->text().toInt();
+  return metadata;
+}
+
+void MetadataDialog::setMetadata(const Metadata &metadata) {
+  ui_->fileName->setText(metadata.file_name_.c_str());
+  ui_->reviewerName->setText(metadata.reviewer_name_.c_str());
+  ui_->tripId->setText(std::to_string(metadata.trip_id_).c_str());
+  ui_->towNumber->setText(std::to_string(metadata.tow_number_).c_str());
+}
+
+#include "../../include/fish_annotator/video_annotator/moc_metadata_dialog.cpp"
+
+}} // namespace fish_annotator::video_annotator
+
