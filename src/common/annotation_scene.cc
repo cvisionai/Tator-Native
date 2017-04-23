dcf363b39768bd32397a96fa072096c6dd0d6ca9
Jonathan Takahashi <jtakahashi@gmail.com>
2017-04-22 23:01:24 -0400

    Add annotation scene class

---
+++ src/common/annotation_scene.cc
@@ -0,0 +1,115 @@
+#include "fish_annotator/common/annotation_scene.h"
+
+namespace fish_annotator {
+
+AnnotationScene::AnnotationScene(QObject *parent)
+  : QGraphicsScene(parent)
+  , mode_(kDoNothing)
+  , type_(kBox)
+  , start_pos_()
+  , rect_item_(nullptr)
+  , line_item_(nullptr)
+  , dot_item_(nullptr) {
+}
+
+AnnotationScene::setToolWidget(AnnotationWidget *widget) {
+  QObject::connect(widget, &AnnotationWidget::setAnnotation,
+      this, &AnnotationScene::setAnnotationType);
+}
+
+AnnotationScene::setMode(Mode mode) {
+  mode_ = mode;
+  QGraphicsView::DragMode drag;
+  if(mode == kDraw) {
+    makeItemsControllable(false);
+    drag = QGraphicsView::NoDrag;
+  }
+  else if(mode == kSelect) {
+    makeItemsControllable(true);
+    drag = QGraphicsView::RubberBandDrag;
+  }
+  auto view = views().at(0);
+  if(view != nullptr) view->setDragMode(drag);
+}
+
+AnnotationScene::setAnnotationType(AnnotationType type) {
+  type_ = type;
+}
+
+AnnotationScene::mousePressEvent(QGraphicsSceneMouseEvent *event) {
+  if(mode_ == kDraw) {
+    start_pos_ = event->scenePos();
+    switch(type_) {
+      case kBox:
+        rect_item_ = new QGraphicsRectItem(QRectF(
+              start_pos_, start_pos_, 0, 0));
+        rect_item_->setPen(QPen(Qt::black, 7, Qt::SolidLine));
+        addItem(rect_item_);
+        break;
+      case kLine:
+        line_item_ = new QGraphicsLineItem(QLineF(
+              start_pos_, start_pos_));
+        line_item_->setPen(QPen(Qt::black, 7, Qt::SolidLine));
+        addItem(line_item_);
+        break;
+      case kDot:
+        dot_item_ = new QGraphicsEllipseItem(QRectF(
+              start_pos_, start_pos_, 0, 0));
+        dot_item_->setPen(QPen(Qt::black, 7, Qt::SolidLine));
+        addItem(dot_item_);
+        break;
+    }
+  }
+  QGraphicsScene::mousePressEvent(event);
+}
+
+AnnotationScene::mouseMoveEvent(QGraphicsSceneMouseEvent *event) {
+  if(mode_ == kDraw) {
+    auto update_pos = event->scenePos();
+    switch(type_) {
+      case kBox:
+        if(rect_item_ != nullptr) {
+          auto left = qMin(start_pos_.x(), update_pos.x());
+          auto top = qMin(start_pos_.y(), update_pos.y());
+          auto width = qAbs(start_pos_.x() - update_pos.x());
+          auto height = qAbs(start_pos_.y() - update_pos.y());
+          rect_item_->setRect(QRectF(left, top, width, height));
+        }
+        break;
+      case kLine:
+        if(line_item_ != nullptr) {
+          line_item_->setLine(QLineF(start_pos_, update_pos));
+        }
+        break;
+      case kDot:
+        if(dot_item_ != nullptr) {
+          dot_item_->setRect(update_pos, update_pos);
+        }
+        break;
+    }
+  }
+}
+
+AnnotationScene::mouseReleaseEvent(QGraphicsSceneMouseEvent *event) {
+  if(mode_ == kDraw) {
+    switch(type_) {
+      case kBox:
+        if(rect_item_ != nullptr) {
+          emit boxFinished(rect_item_->rect());
+        }
+        break;
+      case kLine:
+        if(line_item_ != nullptr) {
+          emit lineFinished(line_item_->line());
+        }
+        break;
+      case kDot:
+        if(dot_item_ != nullptr) {
+          emit dotFinished(dot_item_->rect().topLeft());
+        }
+        break;
+    }
+  }
+}
+
+} // namespace fish_annotator
