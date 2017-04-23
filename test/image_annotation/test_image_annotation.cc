2abd6aa45ab92c1a8f9363389456d7bb187aa4aa
Jonathan Takahashi <jtakahashi@gmail.com>
2017-04-23 17:42:42 -0400

    Integrate annotation types with image annotation test

--- test/image_annotation/test_image_annotation.cc
+++ test/image_annotation/test_image_annotation.cc
@@ -1,6 +1,7 @@
 #include <boost/filesystem.hpp>
 
 #include "fish_annotator/common/rect.h"
+#include "fish_annotator/common/annotation_scene.h"
 #include "fish_annotator/image_annotator/image_annotation.h"
 #include "test_image_annotation.h"
 
@@ -8,6 +9,7 @@
 Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin)
 #endif
 
+namespace fa = fish_annotator;
 namespace ia = fish_annotator::image_annotator;
 
 void TestImageAnnotation::testSerialize() {
@@ -19,16 +21,16 @@ void TestImageAnnotation::testSerialize() {
   ia::ImageAnnotationList list;
   list.insert(std::make_shared<ia::ImageAnnotation>(
     image_list[0].filename().string(), "asdfa", "ljklg", 0, 
-    fish_annotator::Rect(1, 2, 3, 4)));
+    fish_annotator::Rect(1, 2, 3, 4), fa::kBox));
   list.insert(std::make_shared<ia::ImageAnnotation>(
     image_list[0].filename().string(), "ahsdifuasega", "ljsdasdhaklg", 1, 
-    fish_annotator::Rect(3, 2, 1, 9)));
+    fish_annotator::Rect(3, 2, 1, 9), fa::kBox));
   list.insert(std::make_shared<ia::ImageAnnotation>(
     image_list[1].filename().string(), "ahspda8", "asha", 0, 
-    fish_annotator::Rect(4, 3, 2, 8)));
+    fish_annotator::Rect(4, 3, 2, 8), fa::kBox));
   list.insert(std::make_shared<ia::ImageAnnotation>(
     image_list[3].filename().string(), "ahspda8", "ashaahsgha", 0, 
-    fish_annotator::Rect(4, 3, 2, 8)));
+    fish_annotator::Rect(4, 3, 2, 8), fa::kBox));
   QVERIFY(list.getAllSpecies().size() == 3);
   QVERIFY(list.by_file_.left.count(image_list[0].filename().string()) == 2);
   list.write(image_list);
