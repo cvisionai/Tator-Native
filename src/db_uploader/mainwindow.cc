9b3d400ce21e874be490b9d0f044d0d0f06e505e
Jonathan Takahashi <jtakahashi@gmail.com>
2017-09-22 15:51:51 -0400

    Add metadata parsing from path and filename

--- src/db_uploader/mainwindow.cc
+++ src/db_uploader/mainwindow.cc
@@ -12,6 +12,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <fstream>
+std::ofstream debug("DEBUG.txt");
+
 namespace fish_annotator { namespace db_uploader {
 
 namespace fs = boost::filesystem;
@@ -56,8 +59,9 @@ void MainWindow::on_connectOutputDb_clicked() {
       "};DATABASE=" + ui_->outputDatabase->text() + 
       ";Trusted_Connection=no;user_id=" + ui_->outputUsername->text() + 
       ";password=" + ui_->outputPassword->text() + ";WSID=.");
-  output_db_->setUserName(ui_->outputUsername->text());
-  output_db_->setPassword(ui_->outputPassword->text());
+  //@TODO UNCOMMENT THESE FOR CUSTOMER
+  //output_db_->setUserName(ui_->outputUsername->text());
+  //output_db_->setPassword(ui_->outputPassword->text());
   if(output_db_->isValid() == false) {
     ui_->outputDbStatus->setText("Not connected");
     QMessageBox err;
@@ -88,8 +92,8 @@ void MainWindow::on_cancel_clicked() {
 void MainWindow::on_upload_clicked() {
   QString image_dir = ui_->imageDirectory->text();
   std::vector<boost::filesystem::path> image_files;
-  fs::directory_iterator dir_it(image_dir.toStdString());
-  fs::directory_iterator dir_end;
+  fs::recursive_directory_iterator dir_it(image_dir.toStdString());
+  fs::recursive_directory_iterator dir_end;
   for(; dir_it != dir_end; ++dir_it) {
     fs::path ext(dir_it->path().extension());
     for(auto &ok_ext : kDirExtensions) {
@@ -122,9 +126,73 @@ void MainWindow::on_upload_clicked() {
   progress.setCancelButton(0);
   progress.setWindowTitle("Uploading Annotations");
   progress.setMinimumDuration(10);
+  bool ok = true;
   for(int img_index = 0; img_index < num_img; ++img_index) {
+    debug << "IMAGE FILE: " << image_files[img_index] << std::endl;
+    // Parse metadata from directory structure
+    std::vector<std::string> dir_parts;
+    for(const auto &sub : image_files[img_index]) {
+      dir_parts.push_back(sub.string());
+    }
+    auto dir_len = dir_parts.size();
+    if(dir_len < 6) {
+      QMessageBox err;
+      err.critical(0, "Error", 
+          std::string(
+            std::string("Invalid directory structure for image ") +
+            image_files[img_index].string() +
+            std::string("!  Aborting...")).c_str());
+      ok = false;
+      break;
+    }
+    std::string filename = dir_parts[dir_len - 1];
+    std::string camera_control_camera_name = dir_parts[dir_len - 3];
+    std::string area_control_short_name = dir_parts[dir_len - 4];
+    std::string area_control_long_name = dir_parts[dir_len - 5];
+    std::string area_control_year = dir_parts[dir_len - 6];
+    debug << "CAMERA NAME: " << camera_control_camera_name << std::endl;
+    debug << "SHORT NAME: " << area_control_short_name << std::endl;
+    debug << "LONG NAME: " << area_control_long_name << std::endl;
+    debug << "YEAR: " << area_control_year << std::endl;
+    // Parse metadata from filename
+    std::vector<std::string> file_parts;
+    std::string file_part;
+    std::istringstream file_stream(filename.c_str());
+    while(std::getline(file_stream, file_part, '_')) {
+      file_parts.push_back(file_part);
+    }
+    auto file_len = file_parts.size();
+    if(file_len < 5) {
+      QMessageBox err;
+      err.critical(0, "Error", 
+          std::string(
+            std::string("Invalid filename for image ") +
+            image_files[img_index].string() +
+            std::string("!  Aborting...")).c_str());
+      ok = false;
+      break;
+    }
+    std::string area_control_station = file_parts[0].substr(0, 3);
+    std::string area_control_quadrat_str = file_parts[1];
+    int area_control_quadrat = 0;
+    if(1 != sscanf_s(
+          area_control_station.c_str(), 
+          "%dQ", 
+          &area_control_quadrat)) {
+      QMessageBox err;
+      err.critical(0, "Error", 
+          std::string(
+            std::string("Could not parse quadrat from filename for image ") +
+            image_files[img_index].string() +
+            std::string("!  Aborting...")).c_str());
+      ok = false;
+      break;
+    }
+    debug << "STATION: " << area_control_station << std::endl;
+    debug << "QUADRAT: " << area_control_quadrat << std::endl;
     progress.setValue(img_index);
     if(progress.wasCanceled()) {
+      ok = false;
       break;
     }
     auto ann = annotations.getImageAnnotations(image_files[img_index]);
@@ -132,9 +200,9 @@ void MainWindow::on_upload_clicked() {
     for(int ai = 0; ai < num_ann; ++ai) {
       auto row_count = output_model.rowCount();
       if(output_model.insertRows(row_count, 1) == false) {
-        progress.close();
         QMessageBox err;
         err.critical(0, "Error", "Unable to insert row into table.");
+        ok = false;
         break;
       }
       // measurementPK
@@ -166,20 +234,27 @@ void MainWindow::on_upload_clicked() {
       // year
       output_model.setData(output_model.index(row_count, 10), "2017");
     }
-    if(output_model.submitAll() == true) {
-      output_model.database().commit();
-    }
-    else {
-      output_model.database().rollback();
-      progress.close();
-      QMessageBox err;
-      err.critical(0, "Error", output_model.lastError().text());
+    if(ok == false) {
       break;
     }
   }
-  progress.setValue(num_img);
-  QMessageBox status;
-  status.information(0, "Success", "Database upload succeeded!");
+  if(ok == true) {
+    ok = output_model.submitAll();
+  }
+  if(ok == true) {
+    output_model.database().commit();
+    progress.setValue(num_img);
+    QMessageBox status;
+    status.information(0, "Success", "Database upload succeeded!");
+  }
+  else {
+    output_model.database().rollback();
+    progress.close();
+    if(output_db_->lastError().isValid()) {
+      QMessageBox err;
+      err.critical(0, "Error", output_model.lastError().text());
+    }
+  }
   output_db_->exec("SET IDENTITY_INSERT dbo.STAGING_MEASUREMENTS OFF");
   if(output_db_->lastError().isValid()) {
     QMessageBox err;
