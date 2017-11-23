a496c58726752a2a6e75741a441294c2ab388d24
Jonathan Takahashi <jtakahashi@gmail.com>
2017-11-22 19:35:58 -0500

    Change global state type to boost variant, add visitor classes

--- src/common/global_state_annotation.cc
+++ src/common/global_state_annotation.cc
@@ -2,6 +2,30 @@
 
 namespace fish_annotator {
 
+std::string GetValueVisitor::operator()(bool val) const {
+  return val ? "true" : "false";
+}
+
+std::string GetValueVisitor::operator()(std::string val) const {
+  return val;
+}
+
+std::string GetCsvValueVisitor::operator()(bool val) const {
+  return val ? "1" : "0";
+}
+
+std::string GetCsvValueVisitor::operator()(std::string val) const {
+  return val;
+}
+
+std::string GetTypeVisitor::operator()(bool val) const {
+  return "bool";
+}
+
+std::string GetTypeVisitor::operator()(std::string val) const {
+  return "string";
+}
+
 GlobalStateAnnotation::GlobalStateAnnotation()
   : states_() 
   , headers_() {
@@ -12,9 +36,13 @@ pt::ptree GlobalStateAnnotation::write() const {
   for(auto state : states_) {
     pt::ptree tree;
     tree.put("state", state.first);
-    tree.put("value", state.second);
+    tree.put("value", boost::apply_visitor(
+      GetValueVisitor(), 
+      state.second));
     tree.put("header", headers_.at(state.first));
-    tree.put("type", "bool");
+    tree.put("type", boost::apply_visitor(
+      GetTypeVisitor(), 
+      state.second));
     arr.push_back(std::make_pair("", tree));
   }
   return arr;
@@ -24,10 +52,18 @@ void GlobalStateAnnotation::read(const pt::ptree &tree) {
   for(auto val : tree) {
     pt::ptree elem = val.second.get_child("");
     auto new_state = elem.get<std::string>("state");
+    auto type_str = elem.get<std::string>("type");
     if(states_.find(new_state) == states_.end()) {
-      states_.insert(std::pair<std::string, bool>(
-        new_state,
-        elem.get<bool>("value")));
+      if(type_str == "bool") {
+        states_.insert(std::pair<std::string, bool>(
+          new_state,
+          elem.get<bool>("value")));
+      }
+      else if(type_str == "string") {
+        states_.insert(std::pair<std::string, std::string>(
+          new_state,
+          elem.get<std::string>("value")));
+      }
       auto opt_header = elem.get_optional<std::string>("header");
       if(opt_header != boost::none) {
         headers_.insert(std::pair<std::string, std::string>(
@@ -41,7 +77,12 @@ void GlobalStateAnnotation::read(const pt::ptree &tree) {
       }
     }
     else {
-      states_[new_state] = elem.get<bool>("value");
+      if(type_str == "bool") {
+        states_[new_state] = elem.get<bool>("value");
+      }
+      else if(type_str == "string") {
+        states_[new_state] = elem.get<std::string>("value");
+      }
     }
   }
 }
@@ -59,7 +100,9 @@ std::string GlobalStateAnnotation::writeCsvValues() {
   std::string values;
   for (auto state : states_) {
     values += ",";
-    values += state.second ? "1" : "0";
+    values += boost::apply_visitor(
+      GetCsvValueVisitor(),
+      state.second);
   }
   return values;
 }
